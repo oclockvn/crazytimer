b31b2548cd7f98154fbc9cf71b941526b05e42c4
Quang Phan <oclockvn@gmail.com>
2024-01-16 14:25:34 +0700

    Add project files.

---
+++ CrazyTimer/CountdownTimer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyTimer
+{
+    public class CountdownTimer
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+
+        public int CalcTotalSeconds()
+        {
+            return Hour * 3600 + Minute * 60 + Second;
+        }
+    }
+}
---
+++ CrazyTimer/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CrazyTimer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Countdown Timer Calculator Version 1.0");
+            var countdownTimer = new CountdownTimer();
+
+            Console.Write("Enter the number of hours: ");
+            countdownTimer.Hour = int.Parse(Console.ReadLine()!);
+
+            Console.Write("Enter the number of minutes: ");
+            countdownTimer.Minute = int.Parse(Console.ReadLine()!);
+
+            Console.Write("Enter the number of seconds: ");
+            countdownTimer.Second = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine($"The total number of seconds is: {countdownTimer.CalcTotalSeconds()}");
+        }
+    }
+}
