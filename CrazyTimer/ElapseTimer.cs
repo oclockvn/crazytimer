50ae49833f49144a8d48f8d2e68588a9fe7c634a
Quang Phan <oclockvn@gmail.com>
2024-01-16 14:54:43 +0700

    elapse timer

---
+++ CrazyTimer/ElapseTimer.cs
@@ -0,0 +1,72 @@
+﻿namespace CrazyTimer
+{
+    public enum TimeType
+    {
+        Hour,
+        Day,
+        Month,
+        Year,
+    }
+
+    public class ElapseTimer
+    {
+        public int Hour { get; set; }
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+
+        public bool SetValue(string input, TimeType type)
+        {
+            if (!int.TryParse(input, out var number))
+            {
+                return false;
+            }
+
+            switch (type)
+            {
+                case TimeType.Hour:
+                    if (number >= 23 || number < 0)
+                        return false;
+                    Hour = number;
+                    break;
+                case TimeType.Day:
+                    if (number < 0 || number > 31)
+                        return false;
+                    Day = number;
+                    break;
+                case TimeType.Month:
+                    if (number < 1 || number > 12)
+                        return false;
+                    Month = number;
+                    break;
+                case TimeType.Year:
+                    if (number < DateTime.Now.Year)
+                        return false;
+                    Year = number;
+                    break;
+                default:
+                    break;
+            }
+
+            return true;
+        }
+
+        public bool CalcElapseSeconds(out double result)
+        {
+            result = 0;
+            var valid = false;
+
+            try
+            {
+                var target = new DateTime(Year, Month, Day, Hour, 0, 0);
+                result = (target - DateTime.Now).TotalSeconds;
+                valid = true;
+            }
+            catch (Exception)
+            {
+            }
+
+            return valid;
+        }
+    }
+}
--- CrazyTimer/Program.cs
+++ CrazyTimer/Program.cs
@@ -5,13 +5,32 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Countdown Timer Calculator Version 1.0");
-            var countdownTimer = new CountdownTimer();
+            //var countdownTimer = new CountdownTimer();
 
-            countdownTimer.Hour = GetNumber("Enter the number of hours: ", 23);
-            countdownTimer.Minute = GetNumber("Enter the number of minutes: ", 59);
-            countdownTimer.Second = GetNumber("Enter the number of seconds: ", 59);
+            //countdownTimer.Hour = GetNumber("Enter the number of hours: ", 23);
+            //countdownTimer.Minute = GetNumber("Enter the number of minutes: ", 59);
+            //countdownTimer.Second = GetNumber("Enter the number of seconds: ", 59);
 
-            Console.WriteLine($"The total number of seconds is: {countdownTimer.CalcTotalSeconds()}");
+            //Console.WriteLine($"The total number of seconds is: {countdownTimer.CalcTotalSeconds()}");
+
+            var elapseTimer = new ElapseTimer();
+            Console.WriteLine("Enter the hour: ");
+            elapseTimer.SetValue(Console.ReadLine()!, TimeType.Hour);
+            Console.WriteLine("Enter the day: ");
+            elapseTimer.SetValue(Console.ReadLine()!, TimeType.Day);
+            Console.WriteLine("Enter the month: ");
+            elapseTimer.SetValue(Console.ReadLine()!, TimeType.Month);
+            Console.WriteLine("Enter the year: ");
+            elapseTimer.SetValue(Console.ReadLine()!, TimeType.Year);
+
+            if (elapseTimer.CalcElapseSeconds(out var result))
+            {
+                Console.WriteLine($"The total number of seconds is: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid datetime");
+            }
         }
 
         static int GetNumber(string msg, int max, int min = 0)
