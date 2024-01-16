36d8dd1f108192ccc720709c0bc26c33f69ae4fc
Quang Phan <oclockvn@gmail.com>
2024-01-16 14:34:04 +0700

    validate input

--- CrazyTimer/Program.cs
+++ CrazyTimer/Program.cs
@@ -7,16 +7,25 @@
             Console.WriteLine("Countdown Timer Calculator Version 1.0");
             var countdownTimer = new CountdownTimer();
 
-            Console.Write("Enter the number of hours: ");
-            countdownTimer.Hour = int.Parse(Console.ReadLine()!);
-
-            Console.Write("Enter the number of minutes: ");
-            countdownTimer.Minute = int.Parse(Console.ReadLine()!);
-
-            Console.Write("Enter the number of seconds: ");
-            countdownTimer.Second = int.Parse(Console.ReadLine()!);
+            countdownTimer.Hour = GetNumber("Enter the number of hours: ", 23);
+            countdownTimer.Minute = GetNumber("Enter the number of minutes: ", 59);
+            countdownTimer.Second = GetNumber("Enter the number of seconds: ", 59);
 
             Console.WriteLine($"The total number of seconds is: {countdownTimer.CalcTotalSeconds()}");
+        }
+
+        static int GetNumber(string msg, int max, int min = 0)
+        {
+            while (true)
+            {
+                Console.Write(msg);
+                if (int.TryParse(Console.ReadLine(), out var number) && min <= number && max >= number)
+                {
+                    return number;
+                }
+
+                Console.WriteLine("Invalid value");
+            }
         }
     }
 }
